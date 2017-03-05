c93b0fad26dc1d39f03ae72f20c100206eaee260
Andrew Garcia <runandrew@gmail.com>
2017-03-04 19:08:47 -0500

    add lowdb in utilities

---
+++ app/utils/database.js
@@ -0,0 +1,21 @@
+// Required libraries
+const low = require('lowdb');
+const storage = require('lowdb/lib/storages/file-sync');
+const path = require('path');
+
+// Required files
+const { getDbPath } = require('./userSettings');
+
+// Get the path to the database
+const dbPath = getDbPath();
+
+// Grab the db at the user settings path
+const db = low(path.join(dbPath, 'memoriaeDB.json'), { storage });
+
+// If it's a fresh database, then write default values
+db.defaults({ pages: [] })
+.write();
+
+module.exports = {
+  db
+};
