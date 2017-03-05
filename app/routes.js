f8464ca629d30dedc8fb30711fb471b873a1e65a
Andrew Garcia <runandrew@gmail.com>
2017-03-04 22:33:14 -0500

    convert Routes to a connected component

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 // @flow
 // Required libraries
 import React from 'react';
-import { Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { connect } from 'react-redux';
 
 // Required files
 import App from './containers/App';
@@ -21,9 +22,25 @@ function homeOnEnter () {
   }
 }
 
-export default (
-  <Route path="/" component={ App }>
-    <IndexRoute component={ HomePage } onEnter={ homeOnEnter }/>
-    <Route path="/newUser" component={ NewUser } />
-  </Route>
+/* -----------------    COMPONENT     ------------------ */
+
+const Routes = () => (
+  <Router history={ hashHistory }>
+    <Route path="/" component={ App }>
+      <IndexRoute component={ HomePage } onEnter={ homeOnEnter }/>
+      <Route path="/newUser" component={ NewUser } />
+    </Route>
+  </Router>
 );
+
+/* -----------------    CONTAINER     ------------------ */
+
+const mapProps = (state) => ({
+  user: state.user,
+});
+
+const mapDispatch = dispatch => ({
+  fetchPages
+});
+
+export default connect(mapProps, mapDispatch)(Routes);
