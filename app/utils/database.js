2f44be669c180b008f1adf43b10c9783ea549c5f
Andrew Garcia <runandrew@gmail.com>
2017-03-12 16:27:11 -0400

    change location of development database

--- app/utils/database.js
+++ app/utils/database.js
@@ -14,7 +14,14 @@ const dbConnect = () => {
   const db = low(path.join(dbPath, 'memoriaeDB.json'), { storage });
 
   // If it's a fresh database, then write default values
-  db.defaults({ pages: [] })
+  db.defaults({ pages: [
+	  {
+		id: 0,
+		title: 'My first page.',
+		text: 'Type your text here',
+		tags: []
+	  }
+  ] })
   .write();
 
   return db;
