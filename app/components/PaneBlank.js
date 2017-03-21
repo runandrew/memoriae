0c7fb2f6de705c53281a147d3cbdcc65a1060fbf
Andrew Garcia <runandrew@gmail.com>
2017-03-20 20:16:08 -0400

    add blank page component

---
+++ app/components/PaneBlank.js
@@ -0,0 +1,16 @@
+// Required libraries
+import React from 'react';
+
+// Required files
+
+/* -----------------    COMPONENT     ------------------ */
+
+const PaneBlank = () => {
+  return (
+    <div className="pane pane-padding">
+      <h1>No more pages, wahhh</h1>
+    </div>
+  );
+};
+
+export default PaneBlank;
